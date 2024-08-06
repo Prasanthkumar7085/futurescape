9129f02ca664ebe35624299b4900e4b22cbc60c3
prasadarao <prasad.a@orotron.com>
2024-08-06 10:56:08 +0530

    Letterize Updated

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -129,3 +129,20 @@ document.addEventListener('DOMContentLoaded', function () {
 //         translateY: 0
 //     });
 
+const test = new Letterize({
+      targets: "#animateMe"
+    });
+
+    var animation = anime.timeline({
+      targets: test.listAll,
+      delay: anime.stagger(50),
+      loop: true
+    });
+
+    animation
+      .add({
+        translateY: -10
+      })
+      .add({
+        translateY: 0
+      });
